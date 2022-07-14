1f3eb17b42a55208ecc92425412548c073b675b5
Haardispro <haardmajmudar2827@gmail.com>
2022-07-14 12:33:01 +0530

    almost done

--- success.py
+++ success.py
@@ -13,14 +13,14 @@ class window(Gtk.Window):
         self.set_icon_from_file("logo.png")
         
         grid = Gtk.Grid(row_spacing = 10, column_spacing=10)
-        heading = Gtk.Label(label="Your file has been compressed\nsuccessfully")
-        #open_file_path = Gtk.Button("Open File Directory")
-        #open_file_path.connect("clicked", self.open_file)
-        #grid.attach(open_file_path, 1, 2, 1, 1)
+
+        heading = Gtk.Label()
+        heading.set_markup("<big>Your file has been compressed successfully ✅</big>")
+        
+        note = Gtk.Label(label="Note:'Low compression' doesn't always compress your pdf.\nUse Medium or High compression instead")
+        grid.attach(note, 1, 2, 1, 1) 
         grid.attach(heading, 1, 1, 1, 1)
         self.add(grid)
-    #def open_file(self, widget):
-        #command = subprocess.Popen(['xdg-open', '.'])
 win = window()
 win.connect("destroy", Gtk.main_quit)
 win.set_resizable(False)
