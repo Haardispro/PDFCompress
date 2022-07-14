90e0a104de2a3da06f5633f36b185036c3a30671
Haardispro <haardmajmudar2827@gmail.com>
2022-07-14 12:32:32 +0530

    functionality done

--- new.py
+++ new.py
@@ -2,7 +2,6 @@ import shutil
 import os
 import os.path
 import subprocess
-import textwrap
 import gi 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GdkPixbuf
@@ -14,10 +13,12 @@ class window(Gtk.Window):
         self.init_ui()
     def init_ui(self):
         self.set_border_width(10)     
+        
         #Grid
         self.grid = Gtk.Grid(row_spacing=10, column_spacing=10)
         self.set_title("PDFCompress")
         self.set_icon_from_file("logo.png")
+
         #images
         self.icon1 = GdkPixbuf.Pixbuf.new_from_file("logo.png")
         self.image1 = Gtk.Image()
@@ -27,20 +28,19 @@ class window(Gtk.Window):
         #inspdf
         self.inspdf = Gtk.Button(label="Insert PDF")
         self.inspdf.connect("clicked", self.insert_pdf)
+        
         #compdf
         compdf = Gtk.Button(label = "Compress")
         compdf.connect("clicked", self.compress_pdf)
+        
         #Radio Buttons
         self.low = Gtk.RadioButton.new_with_label_from_widget(None, "Low Compression")
         self.low.connect("clicked", self.on_toggled, "low")
-
         self.medium = Gtk.RadioButton.new_with_label_from_widget(self.low, "Medium Compression")
         self.medium.connect("clicked", self.on_toggled, "medium")
-
         self.high = Gtk.RadioButton.new_with_label_from_widget(self.low, "High Compression")   
         self.high.connect("clicked", self.on_toggled, "high")
         
-        #self.filename = Gtk.Label()
         #expand
         self.image1.set_hexpand(True)
         self.image1.set_vexpand(True)
@@ -53,29 +53,24 @@ class window(Gtk.Window):
         self.medium.set_hexpand(True)
         self.medium.set_vexpand(True)
         self.high.set_hexpand(True)
-        self.high.set_vexpand(True)#grid stuff
+        self.high.set_vexpand(True)
+        #grid stuff
         self.grid.attach(self.image1, 1, 0, 1, 1)
         self.grid.attach(self.inspdf, 1, 1, 1, 1)
         self.grid.attach(self.low,    0, 3, 1, 1)
         self.grid.attach(self.medium, 1, 3, 1, 1)
         self.grid.attach(self.high,   2, 3, 1, 1)
         self.grid.attach(compdf,      1, 4, 1, 1)
-        #self.grid.attach(self.filename,1, 2, 1, 1)
-        
-        self.add(self.grid)
 
+        self.add(self.grid)
 
     def on_toggled(self, widget, compress):
         if widget.get_active():
-            state = "on"
             if compress == "low":
-                #print("Low")
                 pass
             elif compress == "medium":
-                #print("Medium")
                 pass
             elif compress == "high":
-                #print("High")
                 pass
         else:
             pass
@@ -90,9 +85,7 @@ class window(Gtk.Window):
             Gtk.STOCK_OPEN,
             Gtk.ResponseType.OK,
         )
-
         self.add_filters(self.dialog)
-        
         response = self.dialog.run()
         if response == Gtk.ResponseType.OK:
             self.x = self.dialog.get_filename()
@@ -100,19 +93,17 @@ class window(Gtk.Window):
             self.inspdf.get_children()[0].set_label(txt)
             #with open('input.txt', 'w') as pdf:
              #   pdf.write(self.x)
-
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
-        
         self.dialog.destroy()
        
-
     def add_filters(self, dialog):
         filter_py = Gtk.FileFilter()
         filter_py.set_name("PDF files")
         filter_py.add_mime_type("application/pdf")
         dialog.add_filter(filter_py)
-    def compress_pdf(self, widget):
+    
+    def compress_pdf(self, widget): 
         def get_ghostscript_path():
             gs_names = ['gs', 'gswin32', 'gswin64']
             for name in gs_names:
@@ -123,32 +114,34 @@ class window(Gtk.Window):
         medium_indicator = '/ebook'
         high_indicator = '/screen'
         gs = get_ghostscript_path()
-        input_file_path = self.x     
-        output_file_path = self.x + "_compressed.pdf" 
-        if self.low.get_active():
-            subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
+        try: 
+            input_file_path = self.x     
+            output_file_path = self.x + "_compressed.pdf" 
+            if self.low.get_active():
+                subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
                     '-dPDFSETTINGS={}'.format(low_indicator),
-                    '-dNOPAUSE', '-dQUIET', '-dBATCH',
-                    '-sOutputFile={}'.format(output_file_path),
-                     input_file_path]
-            )
-        if self.medium.get_active():
-            subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
-                    '-dPDFSETTINGS={}'.format(medium_indicator),
-                    '-dNOPAUSE', '-dQUIET', '-dBATCH',
-                    '-sOutputFile={}'.format(output_file_path),
-                     input_file_path]
-            )
-        if self.high.get_active():
-            subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
-                    '-dPDFSETTINGS={}'.format(high_indicator),
-                    '-dNOPAUSE', '-dQUIET', '-dBATCH',
-                    '-sOutputFile={}'.format(output_file_path),
-                     input_file_path]
-            )
-
-
-        subprocess.Popen(['xdg-open', '{}'.format(input_file_path.replace(os.path.basename(input_file_path), ""))])
+                        '-dNOPAUSE', '-dQUIET', '-dBATCH',
+                        '-sOutputFile={}'.format(output_file_path),
+                        input_file_path]
+                )
+            if self.medium.get_active():
+                subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
+                        '-dPDFSETTINGS={}'.format(medium_indicator),
+                        '-dNOPAUSE', '-dQUIET', '-dBATCH',
+                        '-sOutputFile={}'.format(output_file_path),
+                        input_file_path]
+                )
+            if self.high.get_active():
+                subprocess.call([gs, '-sDEVICE=pdfwrite', '-dCompatibilityLevel=1.4',
+                        '-dPDFSETTINGS={}'.format(high_indicator),
+                        '-dNOPAUSE', '-dQUIET', '-dBATCH',
+                        '-sOutputFile={}'.format(output_file_path),
+                        input_file_path]
+                )
+            os.system("python3 success.py")
+            subprocess.Popen(['xdg-open', '{}'.format(input_file_path.replace(os.path.basename(input_file_path), ""))])
+        except AttributeError:
+            os.system("python3 error.py")
 win = window()
 win.connect("destroy", Gtk.main_quit)
 win.set_resizable(False)
